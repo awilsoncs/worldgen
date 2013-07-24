bc0e644e1af58f047fabbc176567fe64095e213e
Jazztoken <aaronwilsonguitar@gmail.com>
2013-07-24 02:03:09 -0400

    Pull scaling out of processing

--- worldgen/dsprocess.py
+++ worldgen/dsprocess.py
@@ -3,13 +3,15 @@ import math
 import random
 
 import numpy as np
+
+import scaling
     
 def process(worldmap):
     """Populate and return a Worldmap with values."""
     seed_corners(worldmap)
     sew_seams(worldmap)
     step(worldmap)
-    scale_array(worldmap)
+    scaling.scale(worldmap, worldmap.ds_generated)
     return worldmap
 
 def step(worldmap):
@@ -117,54 +119,3 @@ def sew_seams(worldmap):
     for x in xrange(worldmap.shape[0]):
         worldmap[x, 0] = copy(north_loc)
         worldmap[x, -1] = copy(south_loc)
-    
-## Post-generation functions
-
-def scale_array(worldmap):
-    """Scale all values in the array to 0.0-1.0 floats."""
-    # find the min/max for each key
-    print "Scaling array..."
-    max_dict = {}
-    min_dict = {}
-
-    for (x, y), location in np.ndenumerate(worldmap):
-        for key in worldmap.ds_generated:
-            if key not in max_dict or location[key] > max_dict[key]:
-                max_dict[key] = location[key]
-            if key not in min_dict or location[key] < min_dict[key]:
-                min_dict[key] = location[key]
-    
-    # Adjust the values
-    for (x, y), location in np.ndenumerate(worldmap):
-        for key in worldmap.ds_generated:
-            value = location[key] - min_dict[key]
-            value = value / (max_dict[key] - min_dict[key])
-            if key is 'elevation':
-                location[key] = elevation(value)
-            else:
-                location[key] = value
-
-def elevation(x):
-    """Return float x adjusted for earthlike elevations"""
-    x = elevation_central(x) + elevation_end(x)
-    if x > 1.0:
-        x = math.floor(x)
-    return x
-
-def elevation_central(x):
-    """Adjust central ranges of float x"""
-    x = x/4.0 - 0.125
-    ## if x is negative, python won't like the math:
-    if x < 0:
-        ## Real root: 
-        ## (-x)**3 = -(x)**3
-        x = (-x)**(1.0 / 3.0)
-        x = -x
-    else:
-        x = x**(1.0 / 3.0)
-    return 0.5 + 0.25*x
-
-def elevation_end(x):
-    """Adjust end ranges of float x"""
-    x = (27.0 / 64.0) * (2*x - 1)**5 + 3.0/64.0
-    return x
---
+++ worldgen/scaling.py
@@ -0,0 +1,51 @@
+import math
+
+import numpy as np
+
+def scale(worldmap, values):
+    """Scale all values in the array to 0.0-1.0 floats."""
+    # find the min/max for each key
+    print "Scaling array..."
+    max_dict = {}
+    min_dict = {}
+    for (x, y), location in np.ndenumerate(worldmap):
+        for key in values:
+            if key not in max_dict or location[key] > max_dict[key]:
+                max_dict[key] = location[key]
+            if key not in min_dict or location[key] < min_dict[key]:
+                min_dict[key] = location[key]
+    
+    # Adjust the values
+    for (x, y), location in np.ndenumerate(worldmap):
+        for key in worldmap.ds_generated:
+            value = location[key] - min_dict[key]
+            value = value / (max_dict[key] - min_dict[key])
+            if key is 'elevation':
+                location[key] = elevation(value)
+            else:
+                location[key] = value
+
+def elevation(x):
+    """Return float x adjusted for earthlike elevations"""
+    x = elevation_central(x) + elevation_end(x)
+    if x > 1.0:
+        x = math.floor(x)
+    return x
+
+def elevation_central(x):
+    """Adjust central ranges of float x"""
+    x = x/4.0 - 0.125
+    ## if x is negative, python won't like the math:
+    if x < 0:
+        ## Real root: 
+        ## (-x)**3 = -(x)**3
+        x = (-x)**(1.0 / 3.0)
+        x = -x
+    else:
+        x = x**(1.0 / 3.0)
+    return 0.5 + 0.25*x
+
+def elevation_end(x):
+    """Adjust end ranges of float x"""
+    x = (27.0 / 64.0) * (2*x - 1)**5 + 3.0/64.0
+    return x
