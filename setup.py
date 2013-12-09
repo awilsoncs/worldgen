f17f537ba76b0899b2f27470a2409dd16e797250
Aaron Wilson <aaronwilsonguitar@gmail.com>
2013-12-09 00:14:59 -0500

    Clean up setup file.

--- setup.py
+++ setup.py
@@ -4,15 +4,13 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'version': '0.1a'
+    'version': '0.1a',
     'description': 'World Generator',
     'author': 'Aaron Wilson',
-    'url': 'URL to get it at',
-    'download_url': 'Where to download it.',
     'author_email': 'aaronwilsonguitar@gmail.com',
-    'version': '0.1',
     'install_requires': ['nose'],
-    'packages': ['worldgen', 'worldgen.algorithm', 'worldgen.worldmaps'],
+    'packages': ['worldgen', 'worldgen.algorithm', 'worldgen.worldmaps',
+                 'numpy'],
     'scripts': [],
     'name': 'worldgen'
 }
