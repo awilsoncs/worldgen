cf5af4483228bc05bbd5d00ecc097fc04c09a1c3
Aaron Wilson <aaronwilsonguitar@gmail.com>
2014-05-23 20:45:42 -0400

    More Cleanup

--- worldgen/dsprocess.py
+++ worldgen/dsprocess.py
@@ -11,46 +11,59 @@ SOUTH_EAST = [-1, 0]
 NORTH_EAST = [-1, -1]
 
 
-def process(worldmap):
-    """Populate and return a Worldmap with values."""
+def process(world_map):    
+    """
+    Construct the world map.
+    @rtype : recarray
+    @param world_map: recarray
+    """
+    
     print "Building Geology..."
-    smoothness = worldmap['smoothness']
+    smoothness = world_map['smoothness']
     ds_process(smoothness)
     scaling.scale(smoothness)
     print "-Processing smoothness"
-    seed_corners(worldmap)
-    sew_seams(worldmap)
+    seed_corners(world_map)
+    sew_seams(world_map)
     for key in worldmaps.ds_generated:
         print "-Processing %s" % key
-        layer = worldmap[key]
-        ds_process(layer, smoothing_layer=worldmap['smoothness'])
+        layer = world_map[key]
+        ds_process(layer, smoothing_layer=world_map['smoothness'])
         scaling.scale(layer)
-    return worldmap
+    return world_map
 
 
-def seed_corners(worldmap):
+def seed_corners(world_map):
     """Get values for the corners of the wm array."""
+
     print "-Seeding corners..."
 
     #Only get the western corners, we're going to seed the eastern ones from these.
-    worldmap[0, 0] = stack_value(worldmap[0, 0])
-    worldmap[0, -1] = stack_value(worldmap[0, -1])
+    world_map[0, 0] = stack_value(world_map[0, 0])
+    world_map[0, -1] = stack_value(world_map[0, -1])
 
 
-def sew_seams(worldmap):
+def sew_seams(world_map):
     """Insert values into the edges of the map to create continuous lines."""
+
     print "-Sewing seams..."
 
     ## Vertical seams
-    worldmap[0, ] = sew_vertical(worldmap[0])
-    worldmap[-1, ] = worldmap[0, ]
+    world_map[0, ] = sew_vertical(world_map[0])
+    world_map[-1, ] = world_map[0, ]
 
     ## Horizontal seams
-    worldmap[:, 0] = worldmap[0, 0]
-    worldmap[:, -1] = worldmap[0, -1]
+    world_map[:, 0] = world_map[0, 0]
+    world_map[:, -1] = world_map[0, -1]
 
 
 def sew_vertical(seam, iteration=1):
+    """
+
+    @param seam:
+    @param iteration:
+    @return:
+    """
     mid_y = midpoint(seam.shape[0])
     #seam[mid_y] = stack_value(seam[mid_y], [seam[0], seam[-1]], iteration)
     for key in worldmaps.ds_generated:
@@ -62,6 +75,14 @@ def sew_vertical(seam, iteration=1):
 
 
 def stack_value(stack, values=None, iteration=1, smoothing=1):
+    """
+
+    @param stack:
+    @param values:
+    @param iteration:
+    @param smoothing:
+    @return:
+    """
     if values is None:
         values = np.ones(stack.size, dtype=[('smoothness', 'float16'),
                                             ('elevation', 'float16'),
@@ -80,6 +101,14 @@ def stack_value(stack, values=None, iteration=1, smoothing=1):
 
 
 def ds_process(layer, iteration=1, smoothing_layer=None):
+    """
+
+    @param layer:
+    @param iteration:
+    @param smoothing_layer:
+    @return:
+    """
+
     mid_x = midpoint(layer.shape[0])
     mid_y = midpoint(layer.shape[1])
 
@@ -113,8 +142,13 @@ def ds_process(layer, iteration=1, smoothing_layer=None):
 
 
 def diamond(layer, iteration, smoothing_layer=None):
-    """Set the center of layer to the average of the four corners, plus random noise.
     """
+
+    @param layer:
+    @param iteration:
+    @param smoothing_layer:
+    """
+
     mid_x = midpoint(layer.shape[0])
     mid_y = midpoint(layer.shape[1])
 
@@ -134,16 +168,17 @@ def diamond(layer, iteration, smoothing_layer=None):
 
 def square(layer, iteration, smoothing_layer=None):
     """
-    Perform the square step of the Diamond-Square algorithm. Assign the results
-    @param layer:
-    @param iteration:
+    Perform the Square step of the Diamond-Square algorithm on layer.
+    @type  layer: array of floats
+    @param layer: a single record of a world map
+    @type  iteration: integer
+    @param iteration: the iteration of the Diamond Square algorithm
+    @type  smoothing_layer: array of floats
     @param smoothing_layer:
     """
+
     mid_x = midpoint(layer.shape[0])
     mid_y = midpoint(layer.shape[1])
-    #Amusing bugs, may re-implement later as an option
-    #layer[mid_x, 0] = get_value([layer[0, 0], layer[-1, 0]], iteration)
-    #layer[0, mid_y] = get_value([layer[0, 0], layer[0, -1]], iteration)
     layer_ne = layer[-1, -1]
     layer_se = layer[-1, 0]
     layer_nw = layer[0, -1]
@@ -166,6 +201,7 @@ def midpoint(length):
     @param length: integer
     @return: integer
     """
+
     if length > 2:
         return int(length * 0.5)
     return 1
@@ -173,8 +209,10 @@ def midpoint(length):
 
 def get_value(values=1, iteration=1, smoothing=1):
     """Return a float of random noise plus average of values.
+
     @rtype : float
     """
+
     if isinstance(values, int):
         value = values
     else:
